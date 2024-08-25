f2641099bff39dfd4f60c8ebab8f56eb45dd2dcf
MatiasMXS <130255970+MatiasMXS@users.noreply.github.com>
2024-08-25 17:14:08 -0300

    FIX FormFinanzas

--- GYMCAT - SI/FormFinanzas.vb
+++ GYMCAT - SI/FormFinanzas.vb
@@ -4,11 +4,14 @@ Public Class FormFinanzas
 	Private _Conexion As Conexion
 	Public Tabla As String = "Tingresos"
 	Public Tabla2 As String = "Tgastos"
+	Public Tabla3 As String = "TTodo"
+	Public vistaDatos3 As DataView
 	Private Sub FormFinanzas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		Dim consulta As String = "SELECT * FROM ingresos"
 		Dim consulta2 As String = "SELECT * FROM gastos"
+		Dim consulta3 As String = "SELECT ID_ingresos as 'ID Pago', fecha_pago, monto, forma_pago, concepto FROM ingresos UNION SELECT ID_gastos, fecha_gasto, monto, forma_pago, concepto FROM gastos ORDER BY fecha_pago"
 
-		_Conexion = New Conexion(consulta, consulta2, Tabla, Tabla2)
+		_Conexion = New Conexion(consulta, consulta2, consulta3, Tabla, Tabla2, Tabla3)
 
 		' Mostrar ingresos por defecto
 		dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
@@ -21,13 +24,14 @@ Public Class FormFinanzas
 		Select Case cbMostrar.SelectedItem.ToString()
 			Case "Ingresos"
 				dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
+				dgvListadoFinanzas.Columns(0).Visible = False
 			Case "Egresos"
 				dgvListadoFinanzas.DataSource = _Conexion.vistaDatos2
+				dgvListadoFinanzas.Columns(0).Visible = False
 			Case "Todos"
-				Dim todosLosMovimientos As DataTable = _Conexion.GymcatDataSet.Tables("Tingresos").Clone()
-				todosLosMovimientos.Merge(_Conexion.GymcatDataSet.Tables("Tingresos"))
-				todosLosMovimientos.Merge(_Conexion.GymcatDataSet.Tables("Tgastos"))
-				dgvListadoFinanzas.DataSource = todosLosMovimientos.DefaultView
+				dgvListadoFinanzas.DataSource = _Conexion.vistaDatos3
+				dgvListadoFinanzas.Columns(0).Visible = False
+
 		End Select
 	End Sub
 
