553954a0cdd01aea70f2c483e0ff030da0343e53
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2024-08-25 17:46:03 -0300

    lo borre sin querer xd

--- GYMCAT - SI/FormFinanzas.vb
+++ GYMCAT - SI/FormFinanzas.vb
@@ -1,38 +1,41 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class FormFinanzas
-	Private _Conexion As Conexion
-	Public Tabla As String = "Tingresos"
-	Public Tabla2 As String = "Tgastos"
+    Private _Conexion As Conexion
+    Public Tabla As String = "Tingresos"
+    Public Tabla2 As String = "Tgastos"
+    Public Tabla3 As String = "TTodo"
+    Public vistaDatos3 As DataView
 
-	Private Sub FormFinanzas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		Dim consulta As String = "SELECT * FROM ingresos"
-		Dim consulta2 As String = "SELECT * FROM gastos"
+    Private Sub FormFinanzas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim consulta As String = "SELECT * FROM ingresos"
+        Dim consulta2 As String = "SELECT * FROM gastos"
+        Dim consulta3 As String = "SELECT ID_ingresos as 'ID Pago', fecha_pago, monto, forma_pago, concepto FROM ingresos UNION SELECT ID_gastos, fecha_gasto, monto, forma_pago, concepto FROM gastos ORDER BY fecha_pago"
 
-		_Conexion = New Conexion(consulta, consulta2, Tabla, Tabla2)
-
-		' Mostrar ingresos por defecto
-		dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
-		dgvListadoFinanzas.Columns(0).Visible = False
+        _Conexion = New Conexion(consulta, consulta2, consulta3, Tabla, Tabla2, Tabla3)
 
 
-		cbMostrar.SelectedIndex = 0
+        ' Mostrar ingresos por defecto
+        dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
+        dgvListadoFinanzas.Columns(0).Visible = False
 
-	End Sub
 
-	Private Sub cbMostrar_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMostrar.SelectedIndexChanged
-		Select Case cbMostrar.SelectedItem.ToString()
-			Case "Ingresos"
-				dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
-			Case "Egresos"
-				dgvListadoFinanzas.DataSource = _Conexion.vistaDatos2
-			Case "Todos"
-				Dim todosLosMovimientos As DataTable = _Conexion.GymcatDataSet.Tables("Tingresos").Clone()
-				todosLosMovimientos.Merge(_Conexion.GymcatDataSet.Tables("Tingresos"))
-				todosLosMovimientos.Merge(_Conexion.GymcatDataSet.Tables("Tgastos"))
-				dgvListadoFinanzas.DataSource = todosLosMovimientos.DefaultView
-		End Select
-	End Sub
+        cbMostrar.SelectedIndex = 0
 
+    End Sub
+
+    Private Sub cbMostrar_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbMostrar.SelectedIndexChanged
+        Select Case cbMostrar.SelectedItem.ToString()
+            Case "Ingresos"
+                dgvListadoFinanzas.DataSource = _Conexion.vistaDatos
+                dgvListadoFinanzas.Columns(0).Visible = False
+            Case "Egresos"
+                dgvListadoFinanzas.DataSource = _Conexion.vistaDatos2
+                dgvListadoFinanzas.Columns(0).Visible = False
+            Case "Todos"
+                dgvListadoFinanzas.DataSource = _Conexion.vistaDatos3
+                dgvListadoFinanzas.Columns(0).Visible = False
+        End Select
+    End Sub
 
 End Class
